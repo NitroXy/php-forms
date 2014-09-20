563fdc7aae2f4c6fa9ab847fe0c4f012cc057047
David Sveningsson <ext@sidvind.com>
2014-09-20 21:07:16 +0200

    form: switching submit -> button type="submit"

--- Form.php
+++ Form.php
@@ -706,7 +706,12 @@ class FormInput implements FormField {
 }
 
 class FormButton extends FormInput {
-
+	public function get_content(array $extra_attr = array()){
+		$attr = array_merge_recursive($extra_attr, $this->attr);
+		$this->pop_attr('value', $attr, $text);
+		$this->pop_attr('icon', $attr, $icon); /* layout reads icon data, puts html back into attr */
+		return "<button "  . $this->serialize_attr($attr) . ">$icon$text</button>";
+	}
 }
 
 class FormUpload extends FormInput {
