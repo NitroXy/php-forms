ecbca8adced681e3a0efa5ed997fe2226228f41c
David Sveningsson <ext@sidvind.com>
2016-07-17 23:22:11 +0200

    test: test for button name false/null

--- tests/fields/FormButtonTest.php
+++ tests/fields/FormButtonTest.php
@@ -63,6 +63,26 @@ class FormButtonTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals('<button id="id_foo" name="foo" type="button">label</button>', $mock->field['foo']->getContent());
 	}
 
+	public function testButtonNameFalse(){
+		$mock = new MockLayout();
+		$form = Form::create('id', function($f){
+			$f->button('label', false);
+		}, ['layout' => $mock]);
+		$this->assertCount(1, $mock->field); /* anonymous button */
+		$this->assertInstanceOf('NitroXy\PHPForms\FormButton', $mock->field[0]);
+		$this->assertEquals('<button type="button">label</button>', $mock->field[0]->getContent(), "False should not yield name attribute");
+	}
+
+	public function testButtonNameNull(){
+		$mock = new MockLayout();
+		$form = Form::create('id', function($f){
+			$f->button('label', null);
+		}, ['layout' => $mock]);
+		$this->assertCount(1, $mock->field); /* anonymous button */
+		$this->assertInstanceOf('NitroXy\PHPForms\FormButton', $mock->field[0]);
+		$this->assertEquals('<button type="button">label</button>', $mock->field[0]->getContent(), "null should not yield name attribute");
+	}
+
 	public function testSubmitName(){
 		$mock = new MockLayout();
 		$form = Form::create('id', function($f){
